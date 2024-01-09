16631618f72f71bb6c5ba9a5e789f7cda91992fd
shipeng.hu <hushipeng1995@gmail.com>
2024-01-09 16:58:03 +0800

    feat: header组件

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,24 @@
+import HeaderNavLinks from '@/data/headerNavLinks'
+import Link from '@/components/Link'
 const Header = () => {
-  return <div></div>
+  return (
+    <header className={'flex items-center justify-between py-10'}>
+      <div>
+        <Link herf={'/'}>logo</Link>
+      </div>
+      <div className={'flex items-center space-x-4 leading-5 sm:space-x-6'}>
+        {HeaderNavLinks.filter((item) => item.href !== '/').map((link) => (
+          <Link
+            className={'hidden font-medium text-gray-900 sm:block'}
+            key={link.title}
+            herf={link.href}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </header>
+  )
 }
 
 export default Header
