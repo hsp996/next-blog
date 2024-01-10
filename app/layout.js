c19ba4591158b26556f203d5689e4f34994567ae
shipeng.hu <hushipeng1995@gmail.com>
2024-01-10 10:08:02 +0800

    feat: 添加暗黑模式

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,20 @@
 import './globals.css'
+import React from 'react'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
-
+import ThemeProviders from '@/components/ThemeProviders'
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={'bg-white text-black antialiased'}>
-        <SectionContainer>
-          <Header />
-          <main className={'mb-auto'}>{children}</main>
-        </SectionContainer>
+    <html lang="en" suppressHydrationWarning>
+      <body className={'bg-white text-black antialiased dark:bg-gray-950 dark:text-white'}>
+        <ThemeProviders>
+          <SectionContainer>
+            <div className={'flex h-screen flex-col justify-between font-sans'}>
+              <Header />
+              <main className={'mb-auto'}>{children}</main>
+            </div>
+          </SectionContainer>
+        </ThemeProviders>
       </body>
     </html>
   )
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import HeaderNavLinks from '@/data/headerNavLinks'
 import Link from '@/components/Link'
+import ThemeSwitch from '@/components/ThemeSwitch'
 const Header = () => {
   return (
     <header className={'flex items-center justify-between py-10'}>
@@ -9,13 +10,14 @@ const Header = () => {
       <div className={'flex items-center space-x-4 leading-5 sm:space-x-6'}>
         {HeaderNavLinks.filter((item) => item.href !== '/').map((link) => (
           <Link
-            className={'hidden font-medium text-gray-900 sm:block'}
+            className={'hidden font-medium text-gray-900 dark:text-gray-100 sm:block'}
             key={link.title}
             herf={link.href}
           >
             {link.title}
           </Link>
         ))}
+        <ThemeSwitch />
       </div>
     </header>
   )
