913d97bf824cdd35e29cc771c1f9ca4abe3be54e
shipeng.hu <hushipeng1995@gmail.com>
2024-02-06 12:01:21 +0800

    style: 测试

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-
 export default function Home() {
-    console.log('page',);
-    console.log('page',);
-  return (
-    <div>
-      page
-    </div>
-  )
+  console.log('page')
+
+  console.log('page')
+
+  return <div>page</div>
 }
